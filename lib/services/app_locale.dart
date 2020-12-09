50c81f6811bc5905e463b65645773ba26ee52190
Hashim Almasry <zmalek2018@gmail.com>
2020-12-09 11:51:12 +0200

    multilingual

---
+++ lib/services/app_locale.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppLocale {
+  Locale locale;
+  Map<String, String> _loadedLocalizedValues;
+
+  AppLocale({this.locale});
+
+  static AppLocale of(BuildContext context) {
+    return Localizations.of(context, AppLocale);
+  }
+
+  Future loadLang() async {
+    String _langFile =
+        await rootBundle.loadString('assets/lang/${locale.languageCode}.json');
+    Map<String, dynamic> loadedValues = jsonDecode(_langFile);
+    _loadedLocalizedValues =
+        loadedValues.map((key, value) => MapEntry(key, value.toString()));
+  }
+
+  String getTranslated(String key) {
+    return _loadedLocalizedValues[key];
+  }
+
+  static const LocalizationsDelegate<AppLocale> delegate = _AppLocaleDelegate();
+}
+
+class _AppLocaleDelegate extends LocalizationsDelegate<AppLocale> {
+  const _AppLocaleDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'ar'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocale> load(Locale locale) async {
+    AppLocale appLocale = AppLocale(locale: locale);
+    await appLocale.loadLang();
+    return appLocale;
+  }
+
+  @override
+  bool shouldReload(covariant LocalizationsDelegate old) => false;
+}
